38173cf84e652ccdd88b1a17a8d46234148906ce
Ugur Biricik <biricik.ugurr@gmail.com>
2025-08-01 11:33:53 +0200

    PIPELINE

--- .github/actions/cf-deploy/entrypoint.sh
+++ .github/actions/cf-deploy/entrypoint.sh
@@ -7,9 +7,9 @@ cf api ${INPUT_API:-$CF_API} ${cf_opts}
 INPUT_USERNAME=${INPUT_USERNAME:-$CF_USERNAME}
 INPUT_PASSWORD=${INPUT_PASSWORD:-$CF_PASSWORD}
 
-# Use service account authentication
-echo "Using service account authentication..."
-cf auth ${INPUT_USERNAME:-$CF_USERNAME} ${INPUT_PASSWORD:-$CF_PASSWORD} --client-credentials
+# Use API token for authentication
+echo "Using API token authentication..."
+cf auth ${INPUT_USERNAME:-$CF_USERNAME} ${INPUT_PASSWORD:-$CF_PASSWORD}
 
 if [ "x${INPUT_CREATESPACE}" = "xtrue" ]; then
   cf create-space ${INPUT_SPACE:-$CF_SPACE} -o ${INPUT_ORG:-$CF_ORG}
