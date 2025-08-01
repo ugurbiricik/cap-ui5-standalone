282cbddd33958d8dea810aa254f7851ce6a2fb84
Ugur Biricik <biricik.ugurr@gmail.com>
2025-08-01 11:36:12 +0200

    PIPELINE

--- .github/actions/cf-deploy/entrypoint.sh
+++ .github/actions/cf-deploy/entrypoint.sh
@@ -12,10 +12,10 @@ echo "Using API token authentication..."
 cf auth ${INPUT_USERNAME:-$CF_USERNAME} ${INPUT_PASSWORD:-$CF_PASSWORD}
 
 if [ "x${INPUT_CREATESPACE}" = "xtrue" ]; then
-  cf create-space ${INPUT_SPACE:-$CF_SPACE} -o ${INPUT_ORG:-$CF_ORG}
+  cf create-space ${INPUT_SPACE:-$CF_SPACE} -o "${INPUT_ORG:-$CF_ORG}"
 fi
 
-cf target -o ${INPUT_ORG:-$CF_ORG} -s ${INPUT_SPACE:-$CF_SPACE}
+cf target -o "${INPUT_ORG:-$CF_ORG}" -s ${INPUT_SPACE:-$CF_SPACE}
 
 cf deploy ${INPUT_MTAFILE} -f
 
